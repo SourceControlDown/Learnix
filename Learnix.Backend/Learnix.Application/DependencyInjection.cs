14c885dd9abd4292e2d959dc179504bb39057723
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-16 01:53:21 +0300

    add DI registration extension to Application layer

---
+++ Learnix.Backend/Learnix.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Learnix.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Learnix.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
