604cafd90c27afd579ccb5a55ef0399f910e89dc
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-16 02:20:07 +0300

    add DI registration to Infrastructure layer

---
+++ Learnix.Backend/Learnix.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Learnix.Application.Common.Interfaces;
+using Learnix.Infrastructure.Persistence;
+using Learnix.Infrastructure.Persistence.Interceptors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Learnix.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddSingleton<AuditableInterceptor>();
+        services.AddSingleton<SoftDeleteInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            var connectionString = configuration.GetConnectionString("Postgres")
+                ?? throw new InvalidOperationException("Connection string 'Postgres' is not configured.");
+
+            options
+                .UseNpgsql(connectionString)
+                .AddInterceptors(
+                    sp.GetRequiredService<AuditableInterceptor>(),
+                    sp.GetRequiredService<SoftDeleteInterceptor>());
+        });
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
