f7db875903fbe87658040832219705bc26c4fb00
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-16 01:51:12 +0300

    add common interfaces to Application layer

---
+++ Learnix.Backend/Learnix.Application/Common/Interfaces/ICacheable.cs
@@ -0,0 +1,7 @@
+﻿namespace Learnix.Application.Common.Interfaces;
+
+public interface ICacheable
+{
+    string CacheKey { get; }
+    TimeSpan Expiry { get; }
+}
---
+++ Learnix.Backend/Learnix.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Learnix.Application.Common.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
