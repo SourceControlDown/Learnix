f31229e11dd2724a597b93e57317774211b4f7fa
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-16 01:51:37 +0300

    add constants to Application layer

---
+++ Learnix.Backend/Learnix.Application/Common/Constants/CacheKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace Learnix.Application.Common.Constants;
+
+public static class CacheKeys
+{
+    public static string PopularCourses => "popular-courses";
+    public static string Course(Guid id) => $"course:{id}";
+    public static string UserAchievements(Guid userId) => $"user-achievements:{userId}";
+    public static string CategoriesAll => "categories:all";
+}
