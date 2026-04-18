d426ab4109174d6a49f78e12c3de9c940dd7d631
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-18 17:56:52 +0300

    add user and refresh token configuration

---
+++ Learnix.Backend/Learnix.Infrastructure/Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Learnix.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Learnix.Infrastructure.Persistence.Configurations;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(rt => rt.Id);
+
+        builder.Property(rt => rt.TokenHash).HasMaxLength(256).IsRequired();
+        builder.Property(rt => rt.ExpiresAt).IsRequired();
+
+        builder.HasOne(rt => rt.User)
+            .WithMany()
+            .HasForeignKey(rt => rt.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(rt => rt.TokenHash).IsUnique();
+        builder.HasIndex(rt => rt.UserId);
+
+        builder.Ignore(rt => rt.DomainEvents);
+    }
+}
---
+++ Learnix.Backend/Learnix.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Learnix.Domain.Constants;
+using Learnix.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Learnix.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.Property(u => u.FirstName)
+            .HasMaxLength(UserConstants.FirstNameMaxLength)
+            .IsRequired();
+
+        builder.Property(u => u.LastName)
+            .HasMaxLength(UserConstants.LastNameMaxLength)
+            .IsRequired();
+
+        builder.Property(u => u.Bio)
+            .HasMaxLength(UserConstants.BioMaxLength);
+
+        builder.Property(u => u.AvatarUrl)
+            .HasMaxLength(UserConstants.AvatarUrlMaxLength);
+
+        builder.Property(u => u.GoogleId)
+            .HasMaxLength(UserConstants.GoogleIdMaxLength);
+
+        builder.HasIndex(u => u.GoogleId)
+            .IsUnique()
+            .HasFilter($"\"{nameof(User.GoogleId)}\" IS NOT NULL");
+
+        builder.Ignore(u => u.DomainEvents);
+    }
+}
