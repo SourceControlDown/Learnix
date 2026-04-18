eb05e52c4eaab0c42013c4cbb36910f78822f533
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-18 17:58:24 +0300

    add mock email sender

---
+++ Learnix.Backend/Learnix.Infrastructure/Services/ConsoleEmailSender.cs
@@ -0,0 +1,23 @@
+﻿using Learnix.Application.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Learnix.Infrastructure.Services;
+
+internal sealed class ConsoleEmailSender(ILogger<ConsoleEmailSender> logger) : IEmailSender
+{
+    public Task SendEmailConfirmationAsync(string toEmail, string firstName, string confirmationLink, CancellationToken ct = default)
+    {
+        logger.LogInformation(
+            "EMAIL [Confirmation]: To={ToEmail}, FirstName={FirstName}, Link={Link}",
+            toEmail, firstName, confirmationLink);
+        return Task.CompletedTask;
+    }
+
+    public Task SendPasswordResetAsync(string toEmail, string firstName, string resetLink, CancellationToken ct = default)
+    {
+        logger.LogInformation(
+            "EMAIL [PasswordReset]: To={ToEmail}, FirstName={FirstName}, Link={Link}",
+            toEmail, firstName, resetLink);
+        return Task.CompletedTask;
+    }
+}
