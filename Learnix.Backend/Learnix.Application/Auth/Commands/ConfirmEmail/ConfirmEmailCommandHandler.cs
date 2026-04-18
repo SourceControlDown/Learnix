41ed855c80d73c41c370a7d3dceeb08f2ccb3056
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-18 17:30:34 +0300

    add confirm email feature

---
+++ Learnix.Backend/Learnix.Application/Auth/Commands/ConfirmEmail/ConfirmEmailCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using FluentResults;
+using Learnix.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace Learnix.Application.Auth.Commands.ConfirmEmail;
+
+internal sealed class ConfirmEmailCommandHandler(IIdentityService identityService)
+    : IRequestHandler<ConfirmEmailCommand, Result>
+{
+    public async Task<Result> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
+    {
+        // Reverse base64-url encoding from email link
+        var decodedToken = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(request.Token));
+        return await identityService.ConfirmEmailAsync(request.UserId, decodedToken, cancellationToken);
+    }
+}
---
+++ Learnix.Backend/Learnix.Application/Auth/Commands/ConfirmEmail/ConfirmEmailValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Learnix.Application.Auth.Commands.ConfirmEmail;
+
+public sealed class ConfirmEmailValidator : AbstractValidator<ConfirmEmailCommand>
+{
+    public ConfirmEmailValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+        RuleFor(x => x.Token).NotEmpty();
+    }
+}
