4c3f46b7c786c6669032a82683e0f0ff47f5d776
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-04-26 17:23:54 +0300

    add UploadsController controller

---
+++ Learnix.Backend/Learnix.API/Controllers/UploadsController.cs
@@ -0,0 +1,23 @@
+﻿using Learnix.API.Extensions;
+using Learnix.Application.Uploads.Commands.RequestUploadUrl;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnix.API.Controllers;
+
+[ApiController]
+[Route("api/uploads")]
+[Authorize]
+public class UploadsController(ISender mediator) : ControllerBase
+{
+    [HttpPost("request-url")]
+    public async Task<IActionResult> RequestUploadUrl(
+        [FromBody] RequestUploadUrlCommand command,
+        CancellationToken ct)
+    {
+        var result = await mediator.Send(command, ct);
+
+        return result.ToActionResult();
+    }
+}
