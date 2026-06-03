a5a345cc183d6625e85bb2044fc41115c2bb00c7
Oleh Bashtovyi <olehbashtovyi@gmail.com>
2026-06-03 17:24:57 +0300

    feat(caching): add ICacheable to categories, featured courses, course detail, public catalog queries

--- Learnix.Backend/Learnix.Application/Common/Constants/CacheKeys.cs
+++ Learnix.Backend/Learnix.Application/Common/Constants/CacheKeys.cs
@@ -4,6 +4,7 @@ public static class CacheKeys
 {
     public static string PopularCourses => "popular-courses";
     public static string Course(Guid id) => $"course:{id}";
+    public static string CoursesFeatured => "courses:featured";
     public static string UserAchievements(Guid userId) => $"user-achievements:{userId}";
     public static string CategoriesAll => "categories:all";
 }
